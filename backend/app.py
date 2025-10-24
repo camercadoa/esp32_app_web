b5f46e02522ec6b4decbf40c5fb800f2eab0d068
Camilo Mercado <mercadoarizacamilo@gmail.com>
2025-10-24 16:07:58 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -19,4 +19,5 @@ from routes.acciones import set_socketio
 set_socketio(socketio)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+
